b6158b4aeed9b251760d9241eb171659e5f03c1a
Fazeel Ahmed <58031420+thefazeelahmed@users.noreply.github.com>
2022-03-28 23:45:57 +0500

    Plus-Minus

---
+++ hacker-rank/1-week-interview-prep/day-001/Mini-Max Sum/index.js
@@ -0,0 +1,47 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function miniMaxSum(arr) {
+    
+    let dupArr = [...arr]; 
+    let dupArrRev = [...arr]; 
+    
+    dupArrRev.sort((a,b)=>b-a).pop()
+    dupArr.sort((a,b)=>a-b).pop()
+    
+    const sum = dupArr.reduce((acc,item)=>item+acc,0)
+    const sumTwo = dupArrRev.reduce((acc,item)=>item+acc,0)    
+    console.log(sum+" "+sumTwo)
+}
+
+function main() {
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    miniMaxSum(arr);
+}
