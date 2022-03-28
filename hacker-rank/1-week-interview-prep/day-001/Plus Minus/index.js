8af1324355c95dd1b4c1237671eb57c422bbbbde
Fazeel Ahmed <58031420+thefazeelahmed@users.noreply.github.com>
2022-03-28 23:43:56 +0500

    Plus-Minus

---
+++ hacker-rank/1-week-interview-prep/day-001/Plus Minus/index.js
@@ -0,0 +1,49 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'plusMinus' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function plusMinus(arr) {
+    // Write your code here
+    
+    let pos=0,neg=0,zeros=0;
+    
+    arr.forEach((item) => item > 0 ? pos++:(item !== 0 ? neg++: zeros++ )
+)
+
+    console.log(pos/arr.length );
+    console.log(neg/arr.length );
+    console.log(zeros/arr.length );
+    
+}
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    plusMinus(arr);
+}
