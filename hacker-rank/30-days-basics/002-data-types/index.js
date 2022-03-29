92bf689052f48d3af3e1d3c1cf7ad538c50c05e4
Fazeel Ahmed <58031420+thefazeelahmed@users.noreply.github.com>
2022-03-29 09:56:38 +0500

    Loops

---
+++ hacker-rank/30-days-basics/002-data-types/index.js
@@ -0,0 +1,16 @@
+ // Declare second integer, double, and String variables.
+ let secInteger,secDouble,secString;
+ // Read and save an integer, double, and String to your variables.
+         secInteger=parseInt(readLine());
+         secDouble=parseFloat(readLine());
+         secString=readLine();
+
+
+ // Print the sum of both integer variables on a new line.
+     console.log(i+secInteger);
+ // Print the sum of the double variables on a new line.
+     console.log((secDouble+d).toFixed(1));
+     
+ // Concatenate and print the String variables on a new line
+ // The 's' variable above should be printed first.
+     console.log(s+secString);
---
+++ hacker-rank/30-days-basics/003-operators/index.js
@@ -0,0 +1,49 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'solve' function below.
+ *
+ * The function accepts following parameters:
+ *  1. DOUBLE meal_cost
+ *  2. INTEGER tip_percent
+ *  3. INTEGER tax_percent
+ */
+
+function solve(meal_cost, tip_percent, tax_percent) {
+    // Write your code here
+    let tip = parseInt((meal_cost/100*tip_percent).toFixed(0));
+    let tax = parseInt((meal_cost/100*tax_percent).toFixed(0));
+    
+     console.log(parseInt(meal_cost+tip+tax))
+
+}
+
+function main() {
+    const meal_cost = parseFloat(readLine().trim());
+
+    const tip_percent = parseInt(readLine().trim(), 10);
+
+    const tax_percent = parseInt(readLine().trim(), 10);
+
+    solve(meal_cost, tip_percent, tax_percent);
+}
