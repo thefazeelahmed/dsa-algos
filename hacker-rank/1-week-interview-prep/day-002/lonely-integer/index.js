89ab9178f91ba39ac25c7af038fff93ecabeceea
Fazeel Ahmed <58031420+thefazeelahmed@users.noreply.github.com>
2022-03-30 10:03:27 +0500

    1-week-interview-lonely-integer

--- hacker-rank/1-week-interview-prep/day-002/lonely-integer/index.js
+++ hacker-rank/1-week-interview-prep/day-002/lonely-integer/index.js
@@ -1,25 +1,25 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
+const fs = require("fs");
 
 process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding("utf-8");
 
-let inputString = '';
+let inputString = "";
 let currentLine = 0;
 
-process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
 });
 
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
 
-    main();
+  main();
 });
 
 function readLine() {
-    return inputString[currentLine++];
+  return inputString[currentLine++];
 }
 
 /*
@@ -30,36 +30,38 @@ function readLine() {
  */
 
 function lonelyinteger(a) {
-    // Write your code here
-    const obj ={};
-    
-    for(var item of a ){
-        if(obj.hasOwnProperty(item)){
-            obj[item]++;
-        }else{
-            obj[item] = 1;
-        }
-    }
-    console.log(obj)
-    
-    for(var prop in obj){
-         if(obj[prop] === 1){
-            return prop
-        }
-    }
+  // Write your code here
+  const obj = {};
 
+  for (var item of a) {
+    if (obj.hasOwnProperty(item)) {
+      obj[item]++;
+    } else {
+      obj[item] = 1;
+    }
+  }
+  console.log(obj);
+
+  for (var prop in obj) {
+    if (obj[prop] === 1) {
+      return prop;
+    }
+  }
 }
 
 function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
-    const n = parseInt(readLine().trim(), 10);
+  const n = parseInt(readLine().trim(), 10);
 
-    const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+  const a = readLine()
+    .replace(/\s+$/g, "")
+    .split(" ")
+    .map((aTemp) => parseInt(aTemp, 10));
 
-    const result = lonelyinteger(a);
+  const result = lonelyinteger(a);
 
-    ws.write(result + '\n');
+  ws.write(result + "\n");
 
-    ws.end();
+  ws.end();
 }
