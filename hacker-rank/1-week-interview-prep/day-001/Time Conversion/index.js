e80f68f62facb02f9cc600522dc25ca67070d25b
Fazeel Ahmed <58031420+thefazeelahmed@users.noreply.github.com>
2022-03-30 10:01:42 +0500

    1-week-interview-time-conversion

--- hacker-rank/1-week-interview-prep/day-001/Time Conversion/index.js
+++ hacker-rank/1-week-interview-prep/day-001/Time Conversion/index.js
@@ -1,25 +1,25 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
+const fs = require("fs");
 
 process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding("utf-8");
 
-let inputString = '';
+let inputString = "";
 let currentLine = 0;
 
-process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
 });
 
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
 
-    main();
+  main();
 });
 
 function readLine() {
-    return inputString[currentLine++];
+  return inputString[currentLine++];
 }
 
 /*
@@ -30,38 +30,36 @@ function readLine() {
  */
 
 function timeConversion(s) {
-    // Write your code here
-    const hourArr = s.split(':');
-    let hours = hourArr.shift();
-    
-    
-    const amPm = s.slice(-2);
-    
-    if(amPm === "PM"){
-        if(hours ==="12"){
-            hours =12;
-        }
-        else{
-        hours = parseInt(hours)+12;
-        }
-    }else{
-        if(hours === "12"){
-            hours="00";
-        }
+  // Write your code here
+  const hourArr = s.split(":");
+  let hours = hourArr.shift();
+
+  const amPm = s.slice(-2);
+
+  if (amPm === "PM") {
+    if (hours === "12") {
+      hours = 12;
+    } else {
+      hours = parseInt(hours) + 12;
     }
-    
-    const str = hourArr.join(":");
-    return (hours+":"+str.substring(0,str.length-2))
+  } else {
+    if (hours === "12") {
+      hours = "00";
+    }
+  }
+
+  const str = hourArr.join(":");
+  return hours + ":" + str.substring(0, str.length - 2);
 }
 
 function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
-    const s = readLine();
+  const s = readLine();
 
-    const result = timeConversion(s);
+  const result = timeConversion(s);
 
-    ws.write(result + '\n');
+  ws.write(result + "\n");
 
-    ws.end();
+  ws.end();
 }
