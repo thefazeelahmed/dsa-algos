5b9aff35e02b9a26010ca9ec903ece57fa02b2fd
Fazeel Ahmed <58031420+thefazeelahmed@users.noreply.github.com>
2022-04-14 06:57:17 +0500

    leet-code-day-0002-remove-element

---
+++ leet-code/day0002-0001-remove-element/index.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === val) {
+      nums.splice(nums.indexOf(nums[i]), 1);
+      i--;
+    }
+  }
+};
