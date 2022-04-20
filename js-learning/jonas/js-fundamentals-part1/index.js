f5fecd6d1af16633248cace0444dd56a77297211
Fazeel Ahmed <58031420+thefazeelahmed@users.noreply.github.com>
2022-04-20 06:51:29 +0500

    js-fundamentals-part-1-logging-something-variables-and-values

--- js-learning/jonas/js-fundamentals-part1/index.js
+++ js-learning/jonas/js-fundamentals-part1/index.js
@@ -1,4 +1,13 @@
 ////////////////////////////////////
 // Linking a JavaScript File
-let js = "amazing";
-console.log(40 + 8 + 23 - 10);
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
+
+////////////////////////////////////
+// Values and Variables
+console.log("Jonas");
+console.log(23);
+let firstName = "Matilda";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
