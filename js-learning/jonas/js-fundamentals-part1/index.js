64bd68fb96eb5294778de907dc4bb040e17f19b3
Fazeel Ahmed <58031420+thefazeelahmed@users.noreply.github.com>
2022-04-20 06:53:50 +0500

    js-fundamentals-part-1-dated-20/4/2022

--- js-learning/jonas/js-fundamentals-part1/index.js
+++ js-learning/jonas/js-fundamentals-part1/index.js
@@ -13,12 +13,12 @@
 // console.log(firstName);
 
 // Variable name conventions
-let jonas_matilda = "JM";
-let $function = 27;
-let person = "jonas";
-let PI = 3.1415;
-let myFirstJob = "Coder";
-let myCurrentJob = "Teacher";
-let job1 = "programmer";
-let job2 = "teacher";
-console.log(myFirstJob);
+// let jonas_matilda = "JM";
+// let $function = 27;
+// let person = "jonas";
+// let PI = 3.1415;
+// let myFirstJob = "Coder";
+// let myCurrentJob = "Teacher";
+// let job1 = "programmer";
+// let job2 = "teacher";
+// console.log(myFirstJob);
